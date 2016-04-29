5808a90790e007446d70bc57306b11d961b5c941
sandralouise <sandra.adamshallie@gmail.com>
2016-04-29 16:29:41 +0000

    Implement sign out

--- spec/features/signing_users_in_spec.rb
+++ spec/features/signing_users_in_spec.rb
@@ -9,10 +9,10 @@ require "rails_helper"
 
     scenario "with valid credentials" do
     visit "/"
-    click_link "Sign in"
+    click_link "Sign In"
     fill_in "Email", with: @john.email
     fill_in "Password", with: @john.password
-    click_button "Log In"
+    click_button "Log in"
 
     expect(page).to have_content("Signed in successfully.")
     expect(page).to have_content("Signed in as #{@john.email}")
