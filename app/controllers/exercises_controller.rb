b96d8a25089dac404f9a436028633759d471b3f2
sandralouise <sandra.adamshallie@gmail.com>
2016-04-30 19:57:15 +0000

    Update exercise controller and add show view

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,7 +1,9 @@
 class ExercisesController < ApplicationController
   
+  # Applied to the show action
+  before_action :set_exercise, except: [:index, :new, :create]
+  
   def index
-    
   end
   
   def new
@@ -18,6 +20,20 @@ class ExercisesController < ApplicationController
       flash[:danger] = "Oops, your workout could not be added.."
       render :new
     end
+  end
+  
+  def show
+  end
+  
+  
+  private
+  
+  def exercise_params
+    params.require(:exercise).permit(:duration_in_min, :workout, :workout_date, :user_id)
+  end
+  
+  def set_exercise
+    @exercise = current_user.exercises.find(params[:id])
   end
   
 end
