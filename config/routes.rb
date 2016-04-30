e8c4e944832fb141a7601d0d0331f95caad70cba
sandralouise <sandra.adamshallie@gmail.com>
2016-04-30 10:46:41 +0000

    Add exercises routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'dashboard/index'
+  
+  
+  # Create nested route for exercises
+  resources :users do
+    resources :exercises
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
