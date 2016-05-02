06b8c6de4cb5e7b7dd5771d20b3994e47113a245
sandralouise <sandra.adamshallie@gmail.com>
2016-05-02 18:43:58 +0000

    Add users to dashboard

--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -1,18 +1,17 @@
 require "rails_helper"
-
   RSpec.feature "Users Sign Up" do
     scenario "with valid credentials" do
 
     visit "/"
     click_link "Sign Up"
     
-    fill_in "First Name", with: "John"
-    fill_in "Last Name", with: "Doe"
+    fill_in "First name", with: "John"
+    fill_in "Last name", with: "Doe"
     fill_in "Email", with: "user@example.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
-    expect(page).to have_content("You have signed up successfully.")
+    expect(page).to have_content("Welcome! You have signed up successfully.")
     expect(page).to have_content("John Doe")
   end
 end
