1782607ef7fa90b642daceddbfd1cf13d705e2d7
sandralouise <sandra.adamshallie@gmail.com>
2016-05-02 14:05:06 +0000

    List members on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class DashboardController < ApplicationController
+  
   def index
+    @athletes = User.all
   end
+  
 end
---
+++ spec/features/listing_members_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature "Listing Members" do
+  before do
+    @john = User.create(first_name: "John", last_name: "Doe", email: "johndoe@example.com", password: "password")
+    @tess = User.create(first_name: "Tess", last_name: "Doe", email: "tessdoe@example.com", password: "password")
+  end
+  
+  scenario "shows list of registered users" do
+    visit "/"
+    expect(page).to have_content("All Members")
+    expect(page).to have_content(@john.full_name)
+    expect(page).to have_content(@tess.full_name)
+  end
+end
