dbd027d9367795353d395fd0fb8cc9336803e3f6
sandralouise <sandra.adamshallie@gmail.com>
2016-05-03 16:40:39 +0000

    Modify user.rb for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ActiveRecord::Base
   before_save { self.email = email.downcase }
   
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-         
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false },
             length: { maximum: 125 },
             format: { with: VALID_EMAIL_REGEX }
--- spec/features/signing_users_up_spec.rb
+++ spec/features/signing_users_up_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
-  RSpec.feature "Users Sign Up" do
-    scenario "with valid credentials" do
-
+RSpec.feature "Users Sign Up" do
+  scenario "with valid credentials" do
     visit "/"
     click_link "Sign Up"
     
@@ -12,7 +11,20 @@ require "rails_helper"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
     expect(page).to have_content("Welcome! You have signed up successfully.")
-    expect(page).to have_content("John Doe")
+  end
+  
+  scenario "with invalid credentials" do
+    visit "/"
+    click_link "Sign Up"
+    
+    fill_in "First name", with: ""
+    fill_in "Last name", with: ""
+    fill_in "Email", with: "user@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+    expect(page).to have_content("First name can't be blank")
+    expect(page).to have_content("Last name can't be blank")
   end
 end
 
