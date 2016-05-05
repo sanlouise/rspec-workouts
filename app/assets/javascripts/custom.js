6e3c49a5541f7d9c8c0c5d8c933515c491561035
sandralouise <sandra.adamshallie@gmail.com>
2016-05-05 20:12:57 +0000

    Fix navbar bug

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -2,3 +2,9 @@ $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15, // Creates a dropdown of 15 years to control year
 });
+
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
