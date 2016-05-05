4fe07a73a0acfe8dd9275a8e794c9ce47db661db
sandralouise <sandra.adamshallie@gmail.com>
2016-05-05 14:05:39 +0000

    Implement listing friends feature

--- spec/features/listing_exercises_spec.rb
+++ spec/features/listing_exercises_spec.rb
@@ -3,10 +3,15 @@ require 'rails_helper'
 RSpec.feature "Listing Exercises" do
   before do
     @john = User.create(first_name: "John", last_name: "Doe", email: "john@example.com", password: "password")
+    @tess = User.create(first_name: "Tess", last_name: "Doe", email: "tess@example.com", password: "password")
     login_as(@john)
     
     @e1 = @john.exercises.create(duration_in_min: 20, workout: "Running", workout_date: Date.today)
     @e2 = @john.exercises.create(duration_in_min: 60, workout: "Cycling", workout_date: Date.today)
+    
+    # Creates entry in the friendships table
+    @following = Friendship.create(user: @john, friend: @tess)
+  
   end
   
   scenario "shows workouts of past 14 days" do
@@ -18,5 +23,14 @@ RSpec.feature "Listing Exercises" do
     expect(page).to have_content(@e2.duration_in_min)
     expect(page).to have_content(@e2.workout)
     expect(page).to have_content(@e2.workout_date)
+  end
+  
+  scenario "shows memers user follows" do
+    visit "/"
+    click_link "My Lounge"
+    expect(page).to have_content("Members I Follow")
+    expect(page).to have_link(@tess.full_name)
+    expect(page).to have_content(@Unfollow)
+
   end
 end
